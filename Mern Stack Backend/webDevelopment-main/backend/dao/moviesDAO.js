7c7bb4801d0af3be00c0285b11ba2c9c5f3d1bfb
Courts02 <courtneycook2006@gmail.com>
2025-06-24 09:06:09 +0200

    Chapter  13 & 14

--- Mern Stack Backend/webDevelopment-main/backend/dao/moviesDAO.js
+++ Mern Stack Backend/webDevelopment-main/backend/dao/moviesDAO.js
@@ -111,4 +111,14 @@ export default class MoviesDAO {
         }
     }
 
+    // Adds a new movie document to the collection
+    static async addMovie(movieData) {
+        try {
+            return await movies.insertOne(movieData)
+        } catch (e) {
+            console.error(`Unable to add movie: ${e}`)
+            return { error: e }
+        }
+    }
+
 }
