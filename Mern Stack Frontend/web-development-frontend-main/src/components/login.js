6338a6e11b19b7238c196e6923d3a5e9b8d7f71d
Courts02 <courtneycook2006@gmail.com>
2025-06-29 11:37:04 +0200

    Chapters 17 - 21

--- Mern Stack Frontend/web-development-frontend-main/src/components/login.js
+++ Mern Stack Frontend/web-development-frontend-main/src/components/login.js
@@ -1,54 +1,62 @@
 import React, { useState } from 'react';
-import {Form, Button} from 'react-bootstrap';
+// Import Bootstrap Form and Button components
+import { Form, Button } from 'react-bootstrap';
+// Import custom CSS for this component
+import './Login.css';
 
+const Login = (props) => {
+  // State for storing username input
+  const [name, setName] = useState('');
+  // State for storing ID input
+  const [id, setId] = useState('');
 
-const Login = props => {
-   //default state
-   const [name, setName] = useState('')
-   const [id, setId] = useState('')
+  // Handler for username input change
+  const onChangeName = (e) => setName(e.target.value);
 
-   const onChangeName = e => {
-      const name = e.target.value
-      setName(name);
-   }
+  // Handler for ID input change
+  const onChangeId = (e) => setId(e.target.value);
 
-   const onChangeId = e => {
-      const id = e.target.value
-      setId(id);
-   }
+  // Called when user clicks Submit button
+  const login = () => {
+    // Calls login function passed in props with user details
+    props.login({ name, id });
+    // Redirects to the homepage after login
+    props.history.push('/');
+  };
 
-   const login = () => {
-      props.login({ name: name, id: id })
-      props.history.push('/')
-   }
+  return (
+    <div className="login-container">
+      {/* Login Form */}
+      <Form>
+        {/* Username Field */}
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={name}
+            onChange={onChangeName}
+          />
+        </Form.Group>
 
-   return (
-      <div>
-         <Form>
-            <Form.Group>
-               <Form.Label>Username</Form.Label>
-               <Form.Control
-                  type="text"
-                  placeholder="Enter username"
-                  value={name}
-                  onChange={onChangeName}
-               />
-            </Form.Group>
-            <Form.Group>
-               <Form.Label>ID</Form.Label>
-               <Form.Control
-                  type="text"
-                  placeholder="Enter id"
-                  value={id}
-                  onChange={onChangeId}
-               />
-            </Form.Group>
-            <Button variant="primary" onClick={login}>
-               Submit
-            </Button>
-         </Form>
-      </div>
-   )
-}
+        {/* ID Field */}
+        <Form.Group>
+          <Form.Label>ID</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter id"
+            value={id}
+            onChange={onChangeId}
+          />
+        </Form.Group>
+
+        {/* Submit Button */}
+        <Button variant="primary" onClick={login}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
