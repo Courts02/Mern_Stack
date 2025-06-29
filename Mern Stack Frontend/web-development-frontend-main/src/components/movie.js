a69ceb63868e79e38079561761e67c7ecd56ec78
Courts02 <courtneycook2006@gmail.com>
2025-06-29 11:37:10 +0200

    Chapters 17 - 21

--- Mern Stack Frontend/web-development-frontend-main/src/components/movie.js
+++ Mern Stack Frontend/web-development-frontend-main/src/components/movie.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react'
+// Import the movie data service for API calls
 import MovieDataService from '../services/movies'
+// Link component from React Router for navigation
 import { Link } from 'react-router-dom'
-import {Card, Container, Image, Col, Row, Button, Media }from 'react-bootstrap';
-import moment from 'moment';
+// Bootstrap components used in the layout
+import { Card, Container, Image, Col, Row, Button, Media } from 'react-bootstrap'
+// Moment.js for formatting dates nicely
+import moment from 'moment'
+// Custom CSS for the movie component
+import './movie.css'
 
 const Movie = props => {
-   //default state
+   // Local state to store movie details and reviews
    const [movie, setMovie] = useState({
       id: null,
       title: '',
@@ -13,29 +19,30 @@ const Movie = props => {
       reviews: []
    })
 
-   //getting a specific movie's information
+   // Function to fetch movie by ID using the data service
    const getMovie = id => {
       MovieDataService.get(id)
          .then(response => {
+            // Store the fetched movie in local state
             setMovie(response.data)
-            console.log(response.data)
+            console.log(response.data) // Debug: log response
          })
          .catch(e => {
-            console.log(e)
+            console.log(e) // Log any error
          })
    }
 
-   //renders specific movie info once- onfirst render
+   // Runs on component mount & when movie ID param changes
    useEffect(() => {
       getMovie(props.match.params.id)
    }, [props.match.params.id])
 
+   // Deletes a review and updates the movie's local reviews array
    const deleteReview = (reviewId, index) => {
-      //identify review by review and user ids
       MovieDataService.deleteReview(reviewId, props.user.id)
          .then(response => {
+            // Remove the deleted review from local state
             setMovie((prevState) => {
-               //put index into splice method to remove that review from list/database
                prevState.reviews.splice(index, 1)
                return ({
                   ...prevState
@@ -43,62 +50,88 @@ const Movie = props => {
             })
          })
          .catch(e => {
-            //print error message
-            console.log(e)
+            console.log(e) // Log any error
          })
    }
 
    return (
       <div>
-         {/* card to display a specific movie's information */}
          <Container>
             <Row>
+               {/* Movie Poster column */}
                <Col>
                   <Image src={movie.poster + "/100px250"} fluid />
                </Col>
+
+               {/* Movie Info & Reviews column */}
                <Col>
+                  {/* Movie Card with title & plot */}
                   <Card>
                      <Card.Header as="h5">{movie.title}</Card.Header>
                      <Card.Body>
                         <Card.Text>
                            {movie.plot}
                         </Card.Text>
-                        {props.user &&
-                           <Link to={"/movies/" + props.match.params.id + "/review"}>
-                              Add Review
-                           </Link>}
                      </Card.Body>
                   </Card>
-                  <br></br>
+
+                  <br />
+                  {/* Reviews section */}
                   <h2>Reviews</h2>
-                  <br></br>
-                  {/* mapping reviews for movie*/}
-                  {movie.reviews.map((review, index) => {
-                     return (
-                        <Media key={index}>
-                           <Media.Body>
-                              {/* username and date posted. date is formatteed */}
-                              <h5>{review.name + " reviewed on "} {moment(review.date).format("Do MMMM YYYY")}</h5>
-                              <p>{review.review}</p>
-                              {props.user && props.user.id === review.user_id &&
-                                 <Row>
-                                    <Col><Link to={{
-                                       // path for reviews
-                                       pathname: "/movies/" +
-                                          props.match.params.id +
-                                          "/review",
-                                       state: { currentReview: review }
-                                       //link to edit
-                                    }}>Edit</Link>
-                                    </Col>
-                                    {/* link to delete */}
-                                    <Col><Button variant="link" onClick={() => deleteReview(review._id, index)}>Delete</Button></Col>
-                                 </Row>
-                              }
-                           </Media.Body>
-                        </Media>
-                     )
-                  })}
+
+                  <div className="review-box">
+                     {/* Show "+ Add Review" button if user is logged in */}
+                     {props.user &&
+                        <div
+                           style={{
+                              display: 'flex',
+                              justifyContent: 'center',
+                              marginBottom: '20px',
+                           }}
+                        >
+                           <Link to={`/movies/${props.match.params.id}/review`}>
+                              <Button variant="primary">+ Add Review</Button>
+                           </Link>
+                        </div>
+                     }
+
+                     {/* List of reviews */}
+                     {movie.reviews.map((review, index) => {
+                        return (
+                           <Media key={index} className="review-item">
+                              <Media.Body>
+                                 {/* Reviewer name and date formatted with moment */}
+                                 <h5>
+                                    {review.name + " reviewed on "}
+                                    {moment(review.date).format("Do MMMM YYYY")}
+                                 </h5>
+                                 {/* Review text */}
+                                 <p>{review.review}</p>
+
+                                 {/* Show Edit/Delete buttons if logged in user is the author */}
+                                 {props.user && props.user.id === review.user_id &&
+                                    <Row>
+                                       <Col>
+                                          <Link to={{
+                                             pathname: `/movies/${props.match.params.id}/review`,
+                                             state: { currentReview: review }
+                                          }}>Edit</Link>
+                                       </Col>
+                                       <Col>
+                                          <Button
+                                             variant="link"
+                                             onClick={() => deleteReview(review._id, index)}
+                                          >
+                                             Delete
+                                          </Button>
+                                       </Col>
+                                    </Row>
+                                 }
+                              </Media.Body>
+                           </Media>
+                        )
+                     })}
+                  </div>
                </Col>
             </Row>
          </Container>
@@ -106,4 +139,4 @@ const Movie = props => {
    )
 }
 
-export default Movie;
+export default Movie
